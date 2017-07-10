6df078ff1498ceb7b39e88549f7e14287ce880ad
Reshad <r.ahmadi1983@gmail.com>
2017-07-10 09:55:40 +0200

    user creation workds

---
+++ bestoon/apis.md
@@ -0,0 +1,9 @@
+submit/expense
+POST, returns json
+input: date(optional), text, amount
+output: status ok
+
+submit/income
+POST, returns json
+input: date(optional), text, amount
+output: status ok
--- web/models.py
+++ web/models.py
@@ -11,19 +11,27 @@ class Token(models.Model):
         return "{}_token".format(self.user)
 
 
+class Passwordresetcodes(models.Model):
+    code = models.CharField(max_length=32)
+    email = models.CharField(max_length = 120)
+    time = models.DateTimeField()
+    username = models.CharField(max_length=50)
+
+    password = models.CharField(max_length=50) #TODO: do not save password
+
 # Create your models here.
 class Expense(models.Model):
     text  = models.CharField(max_length = 255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User)
-    def __unicode__(self):
-        return "{}, {}".format(self.date, self.amount)
 
+    def __unicode__(self):
+        return "{}-{}-{}".format(self.date,self.user, self.amount)
 class Income(models.Model):
     text  = models.CharField(max_length = 255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User)
     def __unicode__(self):
-        return "{}, {}".format(self.date, self.amount)
+        return "{}-{}-{}".format(self.date,self.user, self.amount)
